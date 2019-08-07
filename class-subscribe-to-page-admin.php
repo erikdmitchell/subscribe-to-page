7cbcacbf1d4e7415a1f6760e90eee10c77c714a1
Erik <erikdmitchell@gmail.com>
2019-08-07 14:19:27 -0400

    admin add email address

--- class-subscribe-to-page-admin.php
+++ class-subscribe-to-page-admin.php
@@ -11,6 +11,8 @@
  */
 class Subscribe_To_Page_Admin {
 
+    public $notices = array();
+
     /**
      * Construct class.
      *
@@ -18,29 +20,10 @@ class Subscribe_To_Page_Admin {
      * @return void
      */
     public function __construct() {
-        $this->includes();
-        $this->init_hooks();
-    }
-
-    /**
-     * Include plugin files.
-     *
-     * @access public
-     * @return void
-     */
-    public function includes() {
-        // include classes.
-    }
-
-    /**
-     * Init hooks for plugin.
-     *
-     * @access private
-     * @return void
-     */
-    private function init_hooks() {
         add_action( 'admin_enqueue_scripts', array( $this, 'admin_scripts_styles' ) );
+        add_action( 'admin_init', array( $this, 'add_email' ) );
         add_action( 'admin_menu', array( $this, 'init' ), 0 );
+        add_action( 'admin_notices', array( $this, 'admin_notices' ) );
     }
 
     /**
@@ -50,7 +33,7 @@ class Subscribe_To_Page_Admin {
      * @return void
      */
     public function init() {
-        add_options_page('Subscribe to Page', 'Subscribe to Page', 'manage_options', 'subscribe-to-page', array($this, 'admin_page'));
+        add_options_page( 'Subscribe to Page', 'Subscribe to Page', 'manage_options', 'subscribe-to-page', array( $this, 'admin_page' ) );
     }
 
     /**
@@ -62,7 +45,7 @@ class Subscribe_To_Page_Admin {
     public function admin_scripts_styles() {
         // nothing.
     }
-    
+
     public function admin_page() {
         $email_list = get_option( 'subscribe_to_post_emails', array() );
         ?>
@@ -73,7 +56,7 @@ class Subscribe_To_Page_Admin {
             <h2>Settings</h2>
             
             <form id="subscribe-to-page-admin-settings" method="post">
-                <?php wp_nonce_field('update_settings', 'stp-admin-settings'); ?>
+                <?php wp_nonce_field( 'update_settings', 'stp-admin-settings' ); ?>
 
                 <table class="form-table">
                     <tbody>
@@ -92,16 +75,22 @@ class Subscribe_To_Page_Admin {
                
             <h2>Email List</h2>
             
-            <form id="subscribe-to-page-admin-email-list" method="post">
+            <div id="subscribe-to-page-admin-email-list">
 
+                <form id="stp-admin-add-email" method="post">
+                    <?php wp_nonce_field( 'add_email', 'stp_admin_add_email' ); ?>
+                    <div class="tablenav bottom">
+                        <div class="alignleft actions bulkactions">
+                            <input type="email" name="email" id="add_email" class="add_email" value="">
+                            <input type="submit" id="doaction" class="button action" value="Add email">
+                        </div>
+                        <br class="clear">
+                    </div> 
+                </form>
+                
                 <table class="wp-list-table widefat fixed striped stp-email-list">
                     <thead>
                         <tr>
-                            <td id="cb" class="manage-column column-cb check-column">
-                                <label class="screen-reader-text" for="cb-select-all-1">Select All</label>
-                                <input id="cb-select-all-1" type="checkbox">
-                            </td>
-                            
                             <th scope="col" id="name" class="manage-column column-email column-primary">
                                 <span>Email</span>
                             </th>
@@ -115,41 +104,66 @@ class Subscribe_To_Page_Admin {
                     <tbody id="the-list">
                         <?php foreach ( $email_list as $key => $email ) : ?>
                             <tr id="email-<?php echo $key; ?>" class="email-<?php echo $key; ?> email hentry">
-                                <th scope="row" class="check-column">
-                                    <label class="screen-reader-text" for="cb-select-<?php echo $key; ?>">Select Email</label>
-                                    <input id="cb-select-<?php echo $key; ?>" type="checkbox" name="stp_emails[]" value="<?php echo $email; ?>">
-                                </th>
-                                
                                 <td class="email column-email column-primary" data-colname="Email">
                                     <div class="row-name"><?php echo $email; ?></div>
                                 </td>
                                 
                                 <td class="actions column-actions" data-colname="Actions">
-                                    <strong><a class="delete" href="#" aria-label="delete">Delete</a></strong>
+                                    <strong><a class="delete" href="<?php echo admin_url( 'options-general.php?page=subscribe-to-page&action=remove-email&emailid=' . $key ); ?>" aria-label="delete">Delete</a></strong>
                                 </td>               
                             </tr>
                         <?php endforeach; ?>
                     </tbody>
-                </table>
-
-                <div class="tablenav bottom">
-                    <div class="alignleft actions bulkactions">
-                        <label for="bulk-action-selector-bottom" class="screen-reader-text">Select bulk action</label>
-                        <select name="action" id="bulk-action-selector-bottom">
-                            <option value="-1">Bulk Actions</option>
-                            <option value="deleteall">Delete</option>
-                        </select>
-                        
-                        <input type="submit" id="doaction" class="button action" value="Apply">
-                    </div>
-                    <br class="clear">
-                </div>
-            </form>
+                </table>               
+            </div>
         </div>
         
         <?php
     }
 
+    public function add_email() {
+        if ( ! isset( $_POST['stp_admin_add_email'] ) || ! wp_verify_nonce( sanitize_key( $_POST['stp_admin_add_email'] ), 'add_email' ) ) {
+            return false;
+        }
+
+        $email = isset( $_POST['email'] ) ? sanitize_text_field( wp_unslash( $_POST['email'] ) ) : '';
+        $add_email = true;
+        $email_list = get_option( 'subscribe_to_post_emails', array() );
+
+        // error if not or invalid email.
+        if ( empty( $email ) || ! is_email( $email ) ) {
+            $this->notices['error'] = 'Invalid email address';
+
+            $add_email = false;
+        }
+
+        // add to email option.
+        if ( $add_email ) {
+            // check if dup.
+            if ( in_array( $email, $email_list ) ) :
+                $this->notices['error'] = 'Already on the list';
+            else :
+                $email_list[] = $email;
+                $this->notices['success'] = 'Email address added';
+            endif;
+        }
+
+        update_option( 'subscribe_to_post_emails', $email_list );
+    }
+
+    public function admin_notices() {
+        if ( empty( $this->notices ) ) {
+            return;
+        }
+
+        foreach ( $this->notices as $notice_class => $notice_message ) :
+            ?>
+            <div class="notice notice-<?php echo $notice_class; ?> is-dismissible">
+                <p><?php echo $notice_message; ?></p>
+            </div>
+            <?php
+        endforeach;
+    }
 }
 
 new Subscribe_To_Page_Admin();
