08456d97f5c2d54af77c992afbb6960f56fc2b52
Erik <erikdmitchell@gmail.com>
2019-08-08 12:14:50 -0400

    added text param for shortcode

--- class-subscribe-to-page-shortcode.php
+++ class-subscribe-to-page-shortcode.php
@@ -34,7 +34,9 @@ class Subscribe_To_Page_Shortcode {
      */
     public function subscribe_to_page_shortcode( $atts ) {
         $atts = shortcode_atts(
-            array(),
+            array(
+                'text' => 'Get email notifications whenever Dell Boomi updates the sub-processors list.'
+            ),
             $atts,
             'subscribe_to_page'
         );
@@ -50,11 +52,14 @@ class Subscribe_To_Page_Shortcode {
             $html .= '<div id="stp-ajax-loader-container"><div class="stp-ajax-loader-spinner"><img src="' . get_site_url( null, '/wp-admin/images/wpspin_light-2x.gif' ) . '" /></div></div>';
             $html .= '<form id="subscribe-to-page-form" class="subscribe-to-page-form" method="post">';
                 $html .= '<div class="subscribe-to-page-form-row">';
-                    $html .= '<label for="email">Email</label><input type="email" id="email" name="email" value="" />';
+                    $html .= '<div class="stp-text">'.$atts['text'].'</div>';
                 $html .= '</div>';
                 $html .= '<div class="subscribe-to-page-form-row">';
-                    $html .= '<input type="checkbox" id="unsubscribe" name="unsubscribe" value="1" /><label for="unsubscribe">Unsubscribe</label>';
+                    $html .= '<input type="email" id="email" name="email" value="" placeholder="Email Address" />';
                 $html .= '</div>';
+                //$html .= '<div class="subscribe-to-page-form-row">';
+                    //$html .= '<input type="checkbox" id="unsubscribe" name="unsubscribe" value="1" /><label for="unsubscribe">Unsubscribe</label>';
+                //$html .= '</div>';
                 $html .= '<button class="subscribe-to-page-form-submit">Subscribe</button>';
             $html .= '</form>';
         $html .= '</div>';
